69ee55998d211ee4a3b97adb6ccd63d62d9c4d0e
theplayboy657-collab <theplayboy657@gmail.com>
2026-08-27 02:53:03 -0400

    Add default config for WA integration

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  prefix: process.env.PREFIX || '!',
+  botName: process.env.BOT_NAME || 'CloneBot',
+  sessionFolder: process.env.SESSION_FOLDER || './session',
+  sessionName: process.env.SESSION_NAME || 'creds.json',
+  sessionID: process.env.SESSION_ID || '',
+};
